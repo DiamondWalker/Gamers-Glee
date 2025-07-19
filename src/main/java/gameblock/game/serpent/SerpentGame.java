ff77148477da74fb980d713333367936080b9f26
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2025-07-19 10:30:18 -0400

    test again

--- src/main/java/gameblock/game/serpent/SerpentGame.java
+++ src/main/java/gameblock/game/serpent/SerpentGame.java
@@ -49,7 +49,7 @@ public class SerpentGame extends Game {
                     snakeDirectionChanged = true;
                 }
             } else {
-                snakeDirection = dir; //
+                snakeDirection = dir;
             }
         } else {
             snakeDirection = dir;
