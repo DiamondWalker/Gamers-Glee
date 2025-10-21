2c73b17ff3c4df5d812e0c9822bafefbb99256fc
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2025-10-21 09:17:27 -0400

    new icon

--- src/main/java/gameblock/datagen/GameblockDataGenerator.java
+++ src/main/java/gameblock/datagen/GameblockDataGenerator.java
@@ -1,13 +1,12 @@
-package gameblock.registry;
+package gameblock.datagen;
 
 import gameblock.GameblockMod;
-import gameblock.datagen.GameblockLootModifierProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = GameblockMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class GameblockDataGenerators {
+public class GameblockDataGenerator {
     @SubscribeEvent
     public static void generateData(GatherDataEvent event) {
         event.getGenerator().addProvider(event.includeServer(), new GameblockLootModifierProvider(event.getGenerator().getPackOutput()));
